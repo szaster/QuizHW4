fff9b503ccf365d868043d534b347c4d65904cff
Svitlana Zaster <szaster@protonmail.com>
2020-05-06 18:05:36 -0500

    Added Questions to .js, added function setTotalPoints.

---
+++ script.js
@@ -0,0 +1,71 @@
+
+let currentQuestion = 0;
+let totalPoints = 0;
+let nAnswers = 4;
+let questions = [
+    {
+        text: "Inside which HTML element do we put the JavaScript?",
+        answers: [
+            "<script>", "<js>", "<javascript>", "<scripting>"
+        ],
+        points: 5,
+        correct: 0
+    },
+    {
+        text: "Where is the correct place to insert a JavaScript?",
+        answers: [
+            "Both the <head> section and the <body> section are correct", "The <head> section", "The <body> section", "None of the above"
+        ],
+        points: 5,
+        correct: 0
+    },
+    {
+        text: "What is the correct syntax for referring to an external script called \"xxx.js\"?",
+        answers: [
+            "<script href=\"xxx.js\">", "<script src=\"xxx.js\">", "<script name=\"xxx.js\">", "None of the above"
+        ],
+        points: 5,
+        correct: 1
+    }
+];
+
+
+function setTotalPoints() {
+    totalPoints = 0;
+    for (i = 0; i < questions.length; i++) {
+        totalPoints += questions[i].points;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function onWindowLoad(){
+    const startButton = document.getElementByIs("startButton");
+    if (startButton !== null) {
+        addEventListener("click", onStartButtonPressed);
+        setTotalPoints();
+    } else {
+        window.alert("Application has failed to load.")
+    }
+
+}
+
+
+window.addEventListener("load", onWindowLoad);
